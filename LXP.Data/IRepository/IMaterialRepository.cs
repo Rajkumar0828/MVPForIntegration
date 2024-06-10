3d5c852a17e205d25ae8b666320d213762dc271e
rajkumar0828 <rajkumar08102001@gmail.com>
2024-06-10 15:53:45 +0530

    mvp

---
+++ LXP.Data/IRepository/IMaterialRepository.cs
@@ -0,0 +1,21 @@
+﻿using LXP.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LXP.Data.IRepository
+{
+    public interface IMaterialRepository
+    {
+        List<Material> GetAllMaterialDetailsByTopicAndType(Topic topic, MaterialType materialType);
+        Task AddMaterial(Material material);
+        Task<bool> AnyMaterialByMaterialNameAndTopic(string materialName, Topic topic);
+        Task<Material> GetMaterialByMaterialNameAndTopic(string materialName, Topic topic);
+
+        Task<Material> GetMaterialById(Guid materialId);
+        Task<List<Material>> GetMaterialsByTopic(Guid topic);
+
+    }
+}
